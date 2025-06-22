db3e439e5e01583429ae195ce73313c2be6d4372
yunhu <703019048@qq.com>
2025-06-22 10:17:57 +0800

    docs: 6

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   // 关闭 eslint
-  lintOnSave: false
+  lintOnSave: false,
+  publicPath: './', // 关键！
 })
-
